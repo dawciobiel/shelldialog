abc2f35d3343ce2513479d3e8df8648cd86b3b3f
Dawid Bielecki <3913996+dawciobiel@users.noreply.github.com>
2026-03-14 19:19:36 +0100

    Docs: add Javadoc for Messages.setLocale and improve localization API

--- src/main/java/org/dawciobiel/shelldialog/console/Messages.java
+++ src/main/java/org/dawciobiel/shelldialog/console/Messages.java
@@ -5,14 +5,29 @@ import java.util.ResourceBundle;
 
 public class Messages {
     private static final String BUNDLE_NAME = "messages";
-    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault());
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault());
 
     private Messages() {
     }
 
+    /**
+     * Sets the locale for the message resource bundle.
+     * This allows changing the language used for retrieving localized strings.<br>
+     * Usage example:<br>
+     * <code>
+     * Messages.setLocale(new Locale("en", "US"));
+     * Messages.setLocale(new Locale("pl", "PL"));
+     * </code>
+     *
+     * @param locale the new locale to be used
+     */
+    public static void setLocale(Locale locale) {
+        resourceBundle = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+    }
+
     public static String getString(String key) {
         try {
-            return RESOURCE_BUNDLE.getString(key);
+            return resourceBundle.getString(key);
         } catch (Exception e) {
             return '!' + key + '!';
         }
